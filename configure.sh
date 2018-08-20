76b8182e222295919e499fba7346a5963dcdce3b
Yeou Sunn <yeou.sunn.liu@gmail.com>
2018-08-20 10:30:23 +0800

    configure.sh

---
+++ configure.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+unamestr=`uname`
+
+
+function downloadJar(){
+	platform
+	if [ ! -d ./libs ]; then
+		mkdir ./libs
+	fi
+
+
+	curl -o ./libs/ulordj-thin-0.0.1-usc-1-bundled.jar -L https://github.com/UlordChain/ulordj-thin/releases/download/ulordj-thin-0.0.1/ulordj-thin-0.0.1-usc-1-bundled.jar;
+
+}
+
+function platform() {
+	if [[ "$unamestr" == 'Linux' ]]; then
+		PLATFORM='linux'
+	elif [[ "$unamestr" == 'Darwin' ]]; then
+		PLATFORM='mac'
+	elif [[ "$unamestr" =~ 'MINGW' ]]; then
+		PLATFORM='windows'
+	else
+			echo -e "\e[1m\e[31m[ ERROR ]\e[0m UNRECOGNIZED PLATFORM"
+			exit 2
+	fi
+}
+
+downloadJar
+
+exit 0
